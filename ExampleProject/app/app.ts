825ce20cfe9e56d8c6133685836fe719170e5866
traing <traingheihong@gmail.com>
2016-09-25 14:41:36 +0200

    ionic sqlite

--- ExampleProject/app/app.ts
+++ ExampleProject/app/app.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
-import { ionicBootstrap, Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
-
-import { HomePage } from './pages/home/home';
-
-
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar, SQLite} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+ 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+    template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
-
-  constructor(public platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+    rootPage: any = HomePage;
+ 
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            StatusBar.styleDefault();
+            let db = new SQLite();
+            db.openDatabase({
+                name: "data.db",
+                location: "default"
+            }).then(() => {
+                db.executeSql("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)", {}).then((data) => {
+                    console.log("TABLE CREATED: ", data);
+                }, (error) => {
+                    console.error("Unable to execute sql", error);
+                })
+            }, (error) => {
+                console.error("Unable to open database", error);
+            });
+        });
+    }
 }
-
+ 
 ionicBootstrap(MyApp);
--- ExampleProject/app/pages/home/home.ts
+++ ExampleProject/app/pages/home/home.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import {SQLite} from "ionic-native";
+ 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-
-  }
+ 
+    public database: SQLite;
+    public people: Array<Object>;
+ 
+    constructor(private navController: NavController, private platform: Platform) {
+        this.database = new SQLite();
+        this.database.openDatabase({name: "data.db", location: "default"}).then(() => {
+            this.refresh();
+        }, (error) => {
+            console.log("ERROR: ", error);
+        });
+    }
+ 
+    public add() {
+        this.database.executeSql("INSERT INTO people (firstname, lastname) VALUES ('Nic', 'Raboy')", []).then((data) => {
+            console.log("INSERTED: " + JSON.stringify(data));
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error.err));
+        });
+    }
+ 
+    public refresh() {
+        this.database.executeSql("SELECT * FROM people", []).then((data) => {
+            this.people = [];
+            if(data.rows.length > 0) {
+                for(var i = 0; i < data.rows.length; i++) {
+                    this.people.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname});
+                }
+            }
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error));
+        });
+    }
+ 
 }
